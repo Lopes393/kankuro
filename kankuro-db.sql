3127ff838fc03d7208a0c939e89d66b8e51882d1
Murilo Lopes <mloopes11@gmail.com>
2023-05-31 21:44:52 -0300

    Project init

---
+++ kankuro-db.sql
@@ -0,0 +1,30 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE `pessoa` (
+    `id` int  NOT NULL ,
+    `name` varchar  NOT NULL ,
+    `function` varchar  NOT NULL ,
+    `email` varchar  NOT NULL ,
+    `cellphone` varchar  NOT NULL ,
+    `id_unidade` int  NOT NULL ,
+    PRIMARY KEY (
+        `id`
+    )
+);
+
+CREATE TABLE `unidade` (
+    `id` int  NOT NULL ,
+    `name` varchar  NOT NULL ,
+    `address` varchar  NOT NULL ,
+    `city` varchar  NOT NULL ,
+    `phone` varchar  NOT NULL ,
+    PRIMARY KEY (
+        `id`
+    )
+);
+
+ALTER TABLE `pessoa` ADD CONSTRAINT `fk_pessoa_id_unidade` FOREIGN KEY(`id_unidade`)
+REFERENCES `unidade` (`id`);
+
